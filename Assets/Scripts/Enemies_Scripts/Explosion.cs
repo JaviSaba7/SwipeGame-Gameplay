09ef03c7941d519bd35608b6c20e5df0a233448c
JaviSaba7 <javiersabariego@hotmail.com>
2018-03-13 15:54:37 +0100

    Swipe Game Gameplay
    -Working on improvements about gameplay, shots...

---
+++ Assets/Scripts/Enemies_Scripts/Explosion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+    public GameObject ball;
+    public GameObject particles_explosion;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "ground")
+        {
+            Debug.Log("explosion");
+            ball.GetComponent<SphereCollider>().radius = 2;
+
+            particles_explosion.SetActive(true);
+            ball.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+}
